44e7fe3fb96ded73ddfcc12db3460d2f6a03a9c3
Gatrhonnx <120284794+gatrhonx@users.noreply.github.com>
2026-07-13 11:28:12 -0400

    Add files via upload

---
+++ .config/hypr/rules.lua
@@ -0,0 +1,9 @@
+
+
+-- Window rules wallpaper-selector
+hl.window_rule({
+    match = { class = "python3", title = "wallpaper-selector" },
+    float = true,
+    center = true,
+    opacity = "0.92 0.92"
+})
